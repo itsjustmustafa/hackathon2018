b8510c491fb7675b71e22ca421065e45d56a0494
Eric Dharmasiri <eric.dharmasiri@gmail.com>
2018-04-21 16:08:42 +1000

    Everything before submit button

--- Student_forms/js/script.js
+++ Student_forms/js/script.js
@@ -1,7 +1,16 @@
 var questions = new Vue({
     el: '#questions',
     data: {
-      yesOrNo: ' ',
-      checkedAnswers: []
+      studentID: '',
+      yesOrNo: '',
+      checkedResponses: []
     }
   })
+
+let session;
+  window.onload = function(){
+    session = null;
+    if(location.href.split('?').length>1){
+      session = location.href.split('?')[1];
+    }
+  }
