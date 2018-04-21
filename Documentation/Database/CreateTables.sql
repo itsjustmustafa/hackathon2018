1eaa886ede61ca54b2321939b7b650778668f5d1
Unknown <jeriah_russo@outlook.com>
2018-04-21 13:53:05 +1000

    Sqlite
    Converted from MySql to Sqlite

--- Documentation/Database/CreateTables.sql
+++ Documentation/Database/CreateTables.sql
@@ -1,42 +1,47 @@
-CREATE TABLE `answers` (
-  `answerID` int(11) NOT NULL AUTO_INCREMENT,
-  `formAnswerID` int(11) NOT NULL,
-  `questionID` int(11) NOT NULL,
-  PRIMARY KEY (`answerID`),
-  KEY `questionID_idx` (`questionID`),
-  CONSTRAINT `questionID` FOREIGN KEY (`questionID`) REFERENCES `questions` (`questionid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE forms (
+    formID   INTEGER NOT NULL
+                     PRIMARY KEY AUTOINCREMENT,
+    formName TEXT    NOT NULL
+                     UNIQUE,
+    formPath TEXT    NOT NULL,
+    formPass         NOT NULL
+);
 
-CREATE TABLE `forms` (
-  `formID` int(11) NOT NULL AUTO_INCREMENT,
-  `formName` varchar(45) NOT NULL,
-  `formPath` varchar(45) NOT NULL,
-  `formPass` varchar(45) NOT NULL,
-  PRIMARY KEY (`formID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE questions (
+    questionID     INTEGER PRIMARY KEY AUTOINCREMENT
+                           NOT NULL,
+    formID                 NOT NULL
+                           REFERENCES forms (formID) ON DELETE RESTRICT
+                                                     ON UPDATE CASCADE,
+    questionFormID INTEGER NOT NULL
+);
 
-CREATE TABLE `questions` (
-  `questionID` int(11) NOT NULL AUTO_INCREMENT,
-  `formQuestionID` int(11) NOT NULL,
-  `formID` int(11) NOT NULL,
-  PRIMARY KEY (`questionID`),
-  KEY `formID_idx` (`formID`),
-  CONSTRAINT `formID` FOREIGN KEY (`formID`) REFERENCES `forms` (`formid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE students (
+    studentID INTEGER PRIMARY KEY
+                    NOT NULL
+                    UNIQUE
+);
 
-CREATE TABLE `responses` (
-  `responseID` int(11) NOT NULL AUTO_INCREMENT,
-  `answerID` int(11) NOT NULL,
-  `responseType` varchar(45) NOT NULL,
-  `responseBool` bit(1) DEFAULT NULL,
-  `responseScale` decimal(10,2) DEFAULT NULL,
-  `responseMultiBool` longtext,
-  PRIMARY KEY (`responseID`),
-  KEY `answerID_idx` (`answerID`),
-  CONSTRAINT `answerID` FOREIGN KEY (`answerID`) REFERENCES `answers` (`answerid`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE answers (
+    answerID     INTEGER PRIMARY KEY AUTOINCREMENT
+                         NOT NULL,
+    questionID   INTEGER NOT NULL
+                         REFERENCES questions (questionID) ON DELETE RESTRICT
+                                                           ON UPDATE CASCADE,
+    formAnswerID INTEGER NOT NULL
+);
 
-CREATE TABLE `students` (
-  `stuID` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`stuID`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+CREATE TABLE responses (
+    responseID        INTEGER         PRIMARY KEY AUTOINCREMENT
+                                      NOT NULL,
+    answerID          INTEGER         REFERENCES answers (answerID) ON DELETE RESTRICT
+                                                                    ON UPDATE CASCADE
+                                      NOT NULL,
+    studentID         INTEGER         REFERENCES students (studentID) ON DELETE RESTRICT
+                                                                      ON UPDATE CASCADE
+                                      NOT NULL,
+    responseType      VARCHAR (45)    NOT NULL,
+    responseBool      BOOLEAN,
+    responseScale     DECIMAL (10, 2),
+    responseMultiBool TEXT
+);
