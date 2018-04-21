49b343a97d62755474814808837de2167a83fe7b
Eric Dharmasiri <eric.dharmasiri@gmail.com>
2018-04-21 20:36:59 +1000

    Form Complete
    Everything is outputted in one string, raw data

--- Student_forms/js/scripts.js
+++ Student_forms/js/scripts.js
@@ -11,23 +11,27 @@ let session;
 }
 
 submitForm = function(event){
-    //event.preventDefault();
-    //console.log(event);
     //studentID
-    console.log(document.getElementById("questions").getElementsByClassName('studentNumber')[0].getElementsByTagName('input')[0].value);
+    var studID = (document.getElementById("questions").getElementsByClassName('studentNumber')[0].getElementsByTagName('input')[0].value);
     //Radiobutton
     if(Number(document.getElementById("questions").getElementsByClassName('radioButton')[0].getElementsByTagName("input")[0].checked) == 1){
-    console.log(Number(true));
+    var radBut = (Number(true));
     }
     else{
-    console.log(Number(false));
+    var radBut = (Number(false));
     }
     //slider
-    console.log(document.getElementById('questions').getElementsByClassName('question')[1].getElementsByClassName('slider')[0].value);
+    var sliderValue = (document.getElementById('questions').getElementsByClassName('question')[1].getElementsByClassName('slider')[0].value);
     //Checkbox
-    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[0].checked));
-    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[1].checked));
-    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[2].checked));
-    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[3].checked));
-    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[4].checked));
+    var checkBoxOne = (Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[0].checked));
+    var checkBoxTwo = (Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[1].checked));
+    var checkBoxThree = (Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[2].checked));
+    var checkBoxFour = (Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[3].checked));
+    var checkBoxFive = (Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[4].checked));
+
+    var checkboxComplete = (checkBoxOne + ", " + checkBoxTwo + ", " + checkBoxThree + ", " + checkBoxFour + ", " + checkBoxFive)
+
+    var completeForm = (studID + ", " + radBut + ", " + sliderValue + ", " + checkboxComplete);
+    
+    console.log(completeForm);
 }
