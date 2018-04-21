fc7869b596b1b111725e9568ba45782fb4906ec4
Eric Dharmasiri <eric.dharmasiri@gmail.com>
2018-04-21 20:14:02 +1000

    Form binary output
    need to convert to a string splitting values with a comma

--- Student_forms/js/scripts.js
+++ Student_forms/js/scripts.js
@@ -12,5 +12,22 @@ let session;
 
 submitForm = function(event){
     //event.preventDefault();
-    console.log(event);
+    //console.log(event);
+    //studentID
+    console.log(document.getElementById("questions").getElementsByClassName('studentNumber')[0].getElementsByTagName('input')[0].value);
+    //Radiobutton
+    if(Number(document.getElementById("questions").getElementsByClassName('radioButton')[0].getElementsByTagName("input")[0].checked) == 1){
+    console.log(Number(true));
+    }
+    else{
+    console.log(Number(false));
+    }
+    //slider
+    console.log(document.getElementById('questions').getElementsByClassName('question')[1].getElementsByClassName('slider')[0].value);
+    //Checkbox
+    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[0].checked));
+    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[1].checked));
+    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[2].checked));
+    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[3].checked));
+    console.log(Number(document.getElementById("questions").getElementsByClassName('checkBox')[0].getElementsByTagName("input")[4].checked));
 }
